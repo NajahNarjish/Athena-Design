73c75324f990e85751a5d7ea015c8d1fc55a116f
Syeda Najah Narjish <najah.masum@gmail.com>
2020-11-19 15:26:03 +0100

    first commit

---
+++ src/Component/StayRunning/StayRunning.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StayRunning = () => {
+    return (
+        <div>
+            <h1>This is StayRunning</h1>
+            
+        </div>
+    );
+};
+
+export default StayRunning;
---
+++ src/Component/WhatWeDo/WhatWeDo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WhatWeDo = () => {
+    return (
+        <div>
+            <h1>What we do</h1>
+            
+        </div>
+    );
+};
+
+export default WhatWeDo;
