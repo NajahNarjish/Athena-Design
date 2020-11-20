78dbe2426fbe182405c68ecdab37d937c3d0953e
Khadiza Rafa <khadiza301rafa@gmail.com>
2020-11-20 21:37:50 +0600

    team and acheivement added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import Achievements from './Component/Achievements/Achievements';
 import DedicatedTeams from './Component/DedicatedTeams/DedicatedTeams';
 import StayRunning from './Component/StayRunning/StayRunning';
 import WhatWeDo from './Component/WhatWeDo/WhatWeDo';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderNav from './Component/Header/HeaderNav/HeaderNav';
+import Banner from './Component/Header/Banner/Banner';
 
 function App() {
   return (
-    <div>
-      <WhatWeDo></WhatWeDo>
-      <StayRunning></StayRunning>
+    <Router>
+      <HeaderNav/>
+      <Banner/>
+      <WhatWeDo/>
+      <StayRunning/>
       <Achievements></Achievements>
       <DedicatedTeams></DedicatedTeams>
-
-      
-    </div>
+    </Router>   
   );
 }
 
